1e42dad923d613407a5e997024e4188096ede0ef
Austin <austin@r00ks.io>
2022-01-26 14:25:56 -0600

    fix mobile post formatting

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -21,7 +21,7 @@ const LayoutWrapper = ({ children }: Props) => {
   const stuckClasses =
     'flex items-center justify-between py-2 sticky top-n-1 z-50 transition-all backdrop isSticky px-4 mx-auto sm:px-6 md:px-[10%] border-b border-slate-900/10 dark:border-slate-300/10 mb-16 w-full'
   const unstuckClasses =
-    'flex items-center justify-between py-8 sticky top-n-1 z-50 transition-all backdrop px-4 mx-auto sm:px-6 md:px-[10%] border-b border-b-0 border-slate-900/10 dark:border-slate-300/10 mb-16 w-full'
+    'flex items-center justify-between py-2 md:py-8 sticky top-n-1 z-50 transition-all backdrop px-4 mx-auto sm:px-6 md:px-[10%] border-b border-b-0 border-slate-900/10 dark:border-slate-300/10 mb-16 w-full'
 
   const classes = stuck ? stuckClasses : unstuckClasses
 
@@ -39,45 +39,43 @@ const LayoutWrapper = ({ children }: Props) => {
 
   return (
     <>
-      <div className="flex flex-col justify-between">
-        <header className={classes} ref={ref}>
-          <div>
-            <Link href="/" aria-label="Tailwind CSS Blog">
-              <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
-                </div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block title">
-                    {siteMetadata.headerTitle}
-                  </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
+      <header className={classes} ref={ref}>
+        <div>
+          <Link href="/" aria-label="Tailwind CSS Blog">
+            <div className="flex items-center justify-between">
+              <div className="mr-3">
+                <Logo />
               </div>
-            </Link>
-          </div>
-          <div className="flex items-center text-base leading-5">
-            <div className="hidden sm:block">
-              {headerNavLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 hover:text-primary-600 dark:hover:text-primary-400"
-                >
-                  {link.title}
-                </Link>
-              ))}
+              {typeof siteMetadata.headerTitle === 'string' ? (
+                <div className="hidden h-6 text-2xl font-semibold sm:block title">
+                  {siteMetadata.headerTitle}
+                </div>
+              ) : (
+                siteMetadata.headerTitle
+              )}
             </div>
-            <ThemeSwitch />
-            <MobileNav />
+          </Link>
+        </div>
+        <div className="flex items-center text-base leading-5">
+          <div className="hidden sm:block">
+            {headerNavLinks.map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 hover:text-primary-600 dark:hover:text-primary-400"
+              >
+                {link.title}
+              </Link>
+            ))}
           </div>
-        </header>
-        <SectionContainer>
-          <main className="mb-auto">{children}</main>
-          <Footer />
-        </SectionContainer>
-      </div>
+          <ThemeSwitch />
+          <MobileNav />
+        </div>
+      </header>
+      <SectionContainer>
+        <main className="mb-auto">{children}</main>
+        <Footer />
+      </SectionContainer>
     </>
   )
 }
