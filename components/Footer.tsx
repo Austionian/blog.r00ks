0858b17a1894fb46a3dd5f0b3c9743f499dbfc97
Austin Rooks <Austionian@users.noreply.github.com>
2022-08-05 16:06:17 -0500

    remove icons from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,12 +6,6 @@ export default function Footer() {
   return (
     <footer>
       <div className="flex flex-col items-center mt-16 mb-8 ">
-        <div className="flex mb-3 space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
-          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
-        </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <Link
             href="https://r00ks.io"
