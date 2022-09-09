8e3860567b03b1de38d9b209dd54245c9b9c5e24
Austin Rooks <Austionian@users.noreply.github.com>
2022-09-09 09:12:21 -0500

    update light theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,7 +27,7 @@ class MyDocument extends Document {
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#000000" />
-          <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
+          <meta name="theme-color" content="#47c3fb" media="(prefers-color-scheme: light)" />
           <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
           <meta charSet="UTF-8" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
